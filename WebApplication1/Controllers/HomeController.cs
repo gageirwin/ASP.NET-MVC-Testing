ad7098daec708cf2d98b9950ecf5f570d4f1c5ea
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2024-02-11 19:30:48 -0600

    Update HomeController.cs

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,20 +15,38 @@ namespace WebApplication1.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public void thingy(string file)
         {
             XmlSerializer serializer = new XmlSerializer(typeof(CATALOG));
-            CATALOG catalog = (CATALOG)serializer.Deserialize(new XmlTextReader("temp/cd_catalog.xml"));
+            XmlTextReader? xmlfile = new XmlTextReader(file);
+
+            if (xmlfile == null) return;
+
+            CATALOG? catalog = serializer.Deserialize(xmlfile) as CATALOG;
+
+            if (catalog == null) return;
+
+            if (catalog.CD == null) return;
 
             foreach (var item in catalog.CD)
             {
-                Console.WriteLine($"Title: {item.TITLE}, Price: {item.PRICE}, Special: {item.SPECIAL}, Options: {item.OPTIONS.THING1}, {item.OPTIONS.THING2}");
+                if (item.OPTIONS == null)
+                    Console.WriteLine($"Title: {item.TITLE}, Price: {item.PRICE}, Special: {item.SPECIAL}");
+                else
+                {
+                    Console.WriteLine($"Title: {item.TITLE}, Price: {item.PRICE}, Special: {item.SPECIAL}, Options: {item.OPTIONS.Length}");
+
+                    foreach (var option in item.OPTIONS)
+                    {
+                        Console.WriteLine($"\tindex: {option.index}, value: {option.Value}");
+                    }
+                }
             }
+        }
 
-
-
-
-
+        public IActionResult Index()
+        {
+            thingy("temp/cd_catalog.xml");
             return View();
         }
 
